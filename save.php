a6496e992c2c0a2417cd86d99d2927cdb41bff6d
Guilherme "Gexis" Romeiro <queilsongui@gmail.com>
2023-09-01 16:04:35 -0300

    asdsad
    AADSAD

--- save.php
+++ save.php
@@ -1,3 +1,34 @@
-<?php
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Processando Postagem</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <div class="painel"> 
+
+        <div class="cabecalho">
+            <img width="200px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/Senac_logo.svg/2560px-Senac_logo.svg.png" alt="">
+            <h1>Connect</h1>    
+        </div>
+
+        <div class="conteudo"> 
+            <h2>Postagem efetuada com Sucesso!</h2>
+            <?php
+                $usuario = "Felipe Geraldino"
+            ?>
+        </div>
+
+        <div class="rodape"> 
+            <a href="index.html" class="botao">Faça outra aqui!</a>
+            <a href="usuario.html" class="botao">Cadastrar Usuário</a>
+            <a href="busca.html" class="botao">Buscar</a>  
+        </div>
+
+    </div>
     
-?>
+</body>
+</html>
