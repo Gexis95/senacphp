643127061bf67a9ff55f2a5f24c135aa8bca19ab
Guilherme "Gexis" Romeiro <queilsongui@gmail.com>
2023-10-05 17:29:58 -0300

    Chat 05/10/2023
    Começo do projeto, adicionado a index e certa personalização

---
+++ chat/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chat</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php 
+    
+    ?>
+
+    <div class="painel">
+        <h1> Senac Connect - Chat com PHP e MYSQL </h1>
+
+        <div class="chat">
+
+        </div>
+        <form method="POST" action="">
+            <input type="text" name="mensagem" placeholder="Digite sua mensagem">
+            <button type="submit">Enviar Mensagem</button>
+        </form>
+
+        <form method="POST" action="">
+            <input type="text" name="usuario" placeholder="Escolha um nome de usuário">
+            <button type="submit">Atualizar nome</button>
+        </form>
+    </div>
+</body>
+</html>
