b3efabf1bbbc2736ff2d2f1ff91214f6f7be5ccf
Guilherme "Gexis" Romeiro <queilsongui@gmail.com>
2023-10-27 17:29:50 -0300

    27/10/2023
    Parte front end do cadastro feita e começo da listagem.
    PELO AMOR DE DEUS ARRUMA O CSS

--- Sistema Administrativo/index.php
+++ Sistema Administrativo/index.php
@@ -25,7 +25,8 @@
 
             <div class="form-group">
                 <label >Senha</label>
-                <input name="senha" type="password" class="form-control" placeholder="Senha" required>
+                <input name="senha" type="password" class="form-control" placeholder="Senha" id="campoSenha" required>
+                <span toggle="#password-field" class="fa fa-fw fa-eye field_icon botaoMostrar"></span>
             </div>
             <br>
 
@@ -45,6 +46,20 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+     <script>
+        $("body").on('click', '.botaoMostrar', function() {
+
+            $(this).toggleClass("fa-eye fa-eye-slash");
+
+            var input = $("#campoSenha");
+            if (input.attr("type") === "password") {
+                input.attr("type", "text");
+            } else {
+                input.attr("type", "password");
+            }
+
+        });
+    </script>
 </body>
 </html>
 
